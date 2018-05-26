f0046ad6092b4e435e2a9f9345b6a4c57e461d37
Jason Lam <jaylam111@gmail.com>
2018-05-25 22:06:48 -0400

    completeted switch

--- bracketChr.js
+++ bracketChr.js
@@ -1,44 +1,57 @@
-// let bracketChkr = (str) => {
-//   let table = {
-//     '[': 1,
-//     '{': 2,
-//     '(': 3,
-//     ']': -1,
-//     '}': -2,
-//     ')': -3
-//   }
-//   let total = 0
-//   // console.log('str : ', str)
-//   // console.log('table[str[0]] : ', table[str[0]])
-//   let i = 0;
-//   console.log('i : ', i)
-//   while (i < str.length) {
-//     // if (table[str[i]]) {
-//       total += table[str[i]]
-//       console.log('total : ', total)
-//     // }
-//     i++;
-//   }
-//   // for (let i = 0; i < str.length; i++) {
-//   //   total += table[str[i]]
-//   // }
-//   console.log('total : ', total)
-//   if (total == 0) {
-//     return true;
-//   } else { return false; }
-
-// }
-
 // switch case
 
 let bracketChkr = (str) => {
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    const element = str[i];
+    switch (element) {
+      case '[':
+        stack.push(element);
 
-switch (key) {
-  case value:
-    
-    break;
+        break;
 
-  default:
-    break;
-}
+      case '(':
+        stack.push(element);
+
+        break;
+
+      case '{':
+        stack.push(element);
+
+        break;
+
+      case ']':
+        if (stack[stack.length - 1] === '[') {
+          stack.pop(element);
+        } else {
+          return false;
+        }
+        break;
+
+      case '}':
+        if (stack[stack.length - 1] === '{') {
+          stack.pop(element);
+        } else {
+          return false;
+        }
+        break;
+
+      case ')':
+        if (stack[stack.length - 1] === '(') {
+          stack.pop(element);
+        } else {
+          return false;
+        }
+        break;
+
+      default:
+        break;
+    }
+
+  }
+  if (!stack.length) {
+    return true;
+  } else {
+      return false;
+  }
 };
