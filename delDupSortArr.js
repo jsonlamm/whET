905122475c9a1fe179e51b13a6c22711919e37ca
Jason Lam <jaylam111@gmail.com>
2018-06-06 21:36:54 -0400

    dup cleanup and addon

--- delDupSortArr.js
+++ delDupSortArr.js
@@ -1,19 +1,22 @@
 let dupRemover = (numArr) => {
-  // let table = {};
-  let tempArr = [];
   for (let i = 0; i < numArr.length; i++) {
     let ele = numArr[i];
-    console.log('ele : ', ele)
-    console.log('tempArr : ', tempArr)
     if (numArr.lastIndexOf(ele) !== i) {
-      console.log('before splice : ', numArr)
       numArr.splice(i, 1)
       i--;
-      console.log('after splice : ', numArr)
     }
-    // else {
-    //   tempArr.push(ele)
-    // }
   }
   return numArr.length
 }
+
+let dupRemover2 = (numArr) => {
+  if (!numArr.length) return 0;
+  let i = 0;
+  for (let j = 0; j < numArr.length; j++){
+      if (numArr[j] !== numArr[i]){
+          i++;
+          numArr[i] = numArr[j];
+      }
+  }
+  return i + 1;
+};
