46db1a48e059a328d74c8398b7ab94397ea71bef
Jason Lam <jaylam111@gmail.com>
2018-06-18 23:43:35 -0400

    final fp counter

--- fp-cCounter.js
+++ fp-cCounter.js
@@ -7,9 +7,10 @@ const MEALS = [
   { description: 'Dinner', calories: 1100 },
 ];
 
-const { td, th, tr, tbody } = tags;
+// unpacking
+const { td, th, tr, tbody, thead, table} = tags;
 
-function cell(tag, className, value) { 
+function cell(tag, className, value) {
   return tag({className}, value);
 }
 
@@ -24,3 +25,36 @@ function mealsBody(className, meals) {
   const rows = R.map(R.partial(mealRow, ['stripe-dark']), meals);
   return tbody({ className }, rows);
 }
+
+// header row consists of 2 cells
+const headerRow = tr([
+  cell(th, 'pa2 tl', 'Meal'),
+  cell(th, 'pa2 tr', 'Calories')
+]);
+
+const mealHeader = thead(headerRow);
+
+// implement es6+ later?
+function totalRow(meals) {
+  const total = R.compose(
+    R.reduce((acc, calories) => acc + calories, 0),
+    R.map(meal => meal.calories)
+  )(meals)
+  return tr({className: 'bt b'}, [
+    cell(td, 'pa2 tr', 'Total'),
+    cell(td, 'pa2 tr', total)
+  ])
+}
+
+function mealDisplay(meals) {
+  return table({className: 'mw5 center w-100 collapse'}, [
+               mealHeader,
+               mealsBody('', meals),
+                totalRow(meals)
+               ])
+}
+const node = document.getElementById('app');
+
+const view = mealDisplay(MEALS);
+
+node.appendChild(view);
